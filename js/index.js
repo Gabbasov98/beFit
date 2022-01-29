e5fac23133271e8a32c538cd5b446bc43b2146d5
Marsel Gabbasov <marsel13_98@mail.ru>
2022-01-29 12:45:20 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+function slider() {
+    var swiper = new Swiper('.feedback .swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        navigation: {
+            nextEl: '.feedback .swiper-button-next',
+            prevEl: '.feedback .swiper-button-prev',
+        },
+        pagination: {
+            el: '.feedback .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                centeredSlides: false,
+            },
+            992: {
+                slidesPerView: 5,
+                centeredSlides: true,
+            },
+        }
+    })
+}
+
+$(document).ready(function() {
+    slider()
+
+    $(".process__item-btn-show").click(function() {
+        $(".process__item").removeClass("process__item--active")
+        $(this).parents(".process__item").addClass("process__item--active")
+        $(".process__item-hidden").slideUp()
+        $(this).parent(".process__item-btn").siblings(".process__item-hidden").slideDown()
+    })
+
+    $(".process__item-btn-hidden").click(function() {
+        $(".process__item").removeClass("process__item--active")
+        $(".process__item-hidden").slideUp()
+    })
+
+    $(".qa__item-btn").click(function() {
+        if ($(this).parents(".qa__item").hasClass("qa__item--active")) {
+            $(".qa__item").removeClass("qa__item--active")
+        } else {
+            $(".qa__item").removeClass("qa__item--active")
+            $(this).parents(".qa__item").addClass("qa__item--active")
+        }
+    })
+})
